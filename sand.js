098dc16478531d658a1202debd5d53ff3449ee86
TanviJadhav26 <76897174+TanviJadhav26@users.noreply.github.com>
2021-02-28 12:56:32 +0530

    Add files via upload

---
+++ sand.js
@@ -0,0 +1,24 @@
+class Sand {
+    constructor(x, y,r) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, r, options);
+      this.r=r;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      fill("brown");
+      ellipse(0, 0,this.r );
+      pop();
+    }
+  };
+  
